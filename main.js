28213e49646aa68ae027808c0a8ce6554cb5c962
Jake Opie <jakeopie9@gmail.com>
2022-02-16 22:40:30 -0600

    base structure added

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// not going to go this route
+// going to build my own intermediate API that takes requests from my JS code,
+// adds my API key, sends a request to the remote API, and then returns my JSON
+// payload back to my JS code. Should be an adventure!
+console.log(process.env.TT_API_K);
---
+++ request.java
@@ -0,0 +1,3 @@
+Class Request {
+    // here is where I think I'll write the code to add the API key 
+}
