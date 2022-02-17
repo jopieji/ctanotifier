7d6e52092c3ec983bde641269cadeece10c06fba
Jake Opie <jakeopie9@gmail.com>
2022-02-16 23:08:25 -0600

    hacky key abstraction implemented

---
+++ apiFiles/request.java
@@ -0,0 +1,12 @@
+//import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.DotenvException;
+package apiFiles;
+
+import apiFiles.sep;
+
+class Request {
+    // here is where I think I'll write the code to add the API key 
+    public static void main(String[] args) {
+        System.out.println(apiFiles.sep.TT_TEST);
+    }
+}
--- request.java
+++
@@ -1,3 +0,0 @@
-Class Request {
-    // here is where I think I'll write the code to add the API key 
-}
