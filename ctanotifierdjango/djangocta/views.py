f37f6c2e067ae212879654b0d733d75d57e271a9
Jake Opie <jakeopie9@gmail.com>
2022-02-20 18:01:03 -0600

    basic json formatting done; dictionary not being displayed or returned

--- ctanotifierdjango/djangocta/views.py
+++ ctanotifierdjango/djangocta/views.py
@@ -7,10 +7,26 @@ import requests
 def index(request):
     # this isn't the URL i need to use; check docs
     # base url: http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx
-    url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&stpid=30126"
+    url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&stpid=30126&outputType=JSON"
     #key = keyHold.TT_API_K
-    key = "keyGoesHere"
-    response = requests.get(url.format(key))
-    print(response.text)
+    key = "keyGoHere"
+    response = requests.get(url.format(key)).json()
+    #print(response.text)
 
+    # might need to generate dictionaries in a loop or store them in a list
+    # more than one dictionary per response
+    ## ctatt is the exterior
+    
+    stop_data = {
+        # stopID will be inserted with variable once its working; just like key is in URL above; just use that variable
+        'requestTime' : response['ctatt']['tmst'],
+        'stopId' : response['ctatt']['eta'][0]['stpId'],
+        'stationName' : response['ctatt']['eta'][0]['staNm'],
+        'destinationName' : response['ctatt']['eta'][0]['destNm'],
+        'predictionTime' : response['ctatt']['eta'][0]['prdt'],
+        'arrivalTime' : response['ctatt']['eta'][0]['arrT'],
+        'approachingBool' : response['ctatt']['eta'][0]['isApp'],
+        'delayedBool' : response['ctatt']['eta'][0]['isDly']
+    }
+    print(stop_data)
     return render(request, 'djangocta/djangocta.html')
