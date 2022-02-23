034461f09accca73d1d35526d84371b09458e761
Jake Opie <jakeopie9@gmail.com>
2022-02-22 22:08:18 -0600

    fix redline stops; add n/s list in dictionary; basic test logic (no buttons)

--- ctanotifierdjango/djangocta/views.py
+++ ctanotifierdjango/djangocta/views.py
@@ -3,56 +3,48 @@ from django.shortcuts import render
 #import keyHold
 import requests
 
-# Create your views here.
-def index(request):
-    # this isn't the URL i need to use; check docs
-    # base url: http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx
-    url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&stpid=30126&outputType=JSON"
-    #key = keyHold.TT_API_K
-    key = "keyHere"
-    response = requests.get(url.format(key)).json()
-    #print(response.text)
-
-    # red line stop id's
+# red line stop id's
     # this probably isn't where these are stored, but for now it's where they are going
-    red_line_ids = {
-        'howard': 40900,
-        'jarvis': 41190,
-        'morse': 40100,
-        'loyola': 41300,
-        'granville': 40760,
-        'thorndale': 40880,
-        'bryn mawr': 41380,
-        'berwyn': 40340,
-        'argyle': 41200,
-        'lawrence': 40770,
-        'wilson': 40540,
-        'sheridan': 40080,
-        'addison': 41420,
-        'belmont': 41320,
-        'fullerton': 41220,
-        'north/clybourn': 40650,
-        'clark/division': 40630,
-        'chicago': 41450,
-        'grand': 40330,
-        'lake': 41660,
-        'monroe': 41090,
-        'jackson': 40560,
-        'harrison': 41490,
-        'roosevelt': 41400,
-        'cermak-chinatown': 41000,
-        'sox-35th': 40190,
-        '47th': 41230,
-        'garfield': 41170,
-        '63rd': 40910,
-        '69th': 40990,
-        '79th': 40240,
-        '87th': 41430,
-        '95th/dan ryan': 40450
-    }
+    # first is northbound, then southbound
+red_line_ids = {
+        'howard': [30173, 30174],
+        'jarvis': [30227, 30228],
+        'morse': [30020, 30021],
+        'loyola': [30251, 30252],
+        'granville': [30147, 30148],
+        'thorndale': [30169, 30170],
+        'bryn mawr': [30267, 30268],
+        'berwyn': [30066, 30067],
+        'argyle': [30229, 30230],
+        'lawrence': [30149, 30150],
+        'wilson': [30105, 30106],
+        'sheridan': [30016, 30017],
+        'addison': [30273, 30274],
+        'belmont': [30255, 30256],
+        'fullerton': [30233, 30234],
+        'north/clybourn': [30125, 30126],
+        'clark/division': [30121, 30122],
+        'chicago': [30279, 30280],
+        'grand': [30064, 30065],
+        'lake': [30289, 30290],
+        'monroe': [30211, 30212],
+        'jackson': [30109, 30110],
+        'harrison': [30285, 30286],
+        'roosevelt': [30269, 30270],
+        'cermak-chinatown': [30193, 30194],
+        'sox-35th': [30036, 30037],
+        '47th': [302387, 30238],
+        'garfield': [30223, 30224],
+        '63rd': [30177, 30178],
+        '69th': [30191, 30192],
+        '79th': [30046, 30047],
+        '87th': [30276, 30275],
+        '95th/dan ryan': [30088, 30089]
+}
 
-    # brown line stop ids: direction is super critical here
-    brn_line_ids = {
+# brown line stop ids: I DID THESE WRONG; THESE ARE MAP ID
+"""
+brn_line_ids = {
         'kimball': 41290,
         'kedzie': 41180,
         'francisco': 40870,
@@ -82,7 +74,25 @@ def index(request):
         'quincy': 40040,
         'washington/wabash': 41700,
         'washington/wells': 40730
-    }
+}
+"""
+
+# Create your views here.
+def index(request):
+    # base url: http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx
+    # test id: 30126 for N/C
+    stop = input("What stop are you wanting data for?\n").lower()
+    ns = input("Northbound or southbound? (type 'n' or 's')\n").lower()
+    trig = 0
+    if ns == "s":
+        trig = 1
+    key = ""
+    desiredStop = red_line_ids.get(stop)[trig]
+    print(desiredStop)
+    url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={}&stpid={}&outputType=JSON"
+    
+    response = requests.get(url.format(key, desiredStop)).json()
+    print(response)
 
     # might need to generate dictionaries in a loop or store them in a list
     # more than one dictionary per response
